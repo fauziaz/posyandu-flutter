674fd6b68302b48ce39cbe08a88ab586290a6884
finadio <finajulianti82@gmail.com>
2025-12-15 15:50:12 +0700

    Menambahkan konfigurasi Supabase

---
+++ lib/utils/supabase_config.dart
@@ -0,0 +1,6 @@
+// utils/supabase_config.dart
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://fatifoamnwbpkvadykin.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhdGlmb2FtbndicGt2YWR5a2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3NTg4NjcsImV4cCI6MjA4MTMzNDg2N30.j865oYDp0YFAP4mu_b1PpiXdjURe_8UeaLkCXBuWlYk';
+}
